9f416f9098b8f78584295de1272aace5ed7dd6f2
Ilan Elbaz <elbazila@octal10.iro.umontreal.ca>
2018-06-13 15:16:19 -0400

    comit

--- etude_signal/code/signal.py
+++ etude_signal/code/signal.py
@@ -94,7 +94,7 @@ def cosine_similarity(vec1, vec2):
 def numeral_position(position, text, bool_normalize = True):
     tab_sent = sent_tokenize(text)
     taille = -1
-    position_word_span = 1
+    position_word_span = 0
     for sent in tab_sent:
         taille += len(sent) + 1
         if position <= taille: #si on a trouver la phrase
@@ -103,7 +103,7 @@ def numeral_position(position, text, bool_normalize = True):
             for word in word_tokenize(sent):
                 taille_mot += len(word)
                 if position <= taille_mot:
-                    print("mot correspondant", word)
+                    # print("mot correspondant", word)
                     return position_word_span
                 if word != "#":
                     position_word_span += 1
@@ -115,7 +115,8 @@ total_answer = 0
 with open(path_data+'dev-v1.1.json', 'r') as input:
     d = json.load(input)
     input.close()
-
+    pareil=0
+    diff=0
     for data in d['data']:
         for paragraph in data['paragraphs']:
             # text_parag_tokens_1 = sent_tokenize(paragraph['context'])
@@ -136,8 +137,25 @@ with open(path_data+'dev-v1.1.json', 'r') as input:
                     #     if total_text_char > pos and do:
                     #         do = False
                     # sentence_positionRelative = numeral_position(pos, paragraph['context'], True)
-                    print("reponse attendu   ", answer['text'])
+                    # print("reponse attendu   ", answer['text'])
                     position_Word = numeral_position(pos, paragraph['context'], True)
+                    # print("mot sorti", )
+                    if(  word_tokenize(answer['text'])[0] != word_tokenize(paragraph['context'])[position_Word]):
+                        # print(word_tokenize(answer['text'])[0])
+                        # print(word_tokenize(paragraph['context'])[position_Word+1])
+                        # print("diff")
+                        print(question['question'])
+                        print('id', question['id'])
+                        print("mot reponse :", answer['text'])
+                        print("position" , position_Word)
+                        print("position_char", pos)
+                        print(paragraph['context'])
+                        print("mot texte", word_tokenize(paragraph['context']))
+                        print("mot sorti", word_tokenize(paragraph['context'])[position_Word])
+                        time.sleep(3)
+                    if (word_tokenize(answer['text'])[0] == word_tokenize(paragraph['context'])[position_Word]):
+                        pareil += 1
+
                     # if not(sentence_positionRelative in list_ans):
                     #     # print("debut")
                     #     print("reponse attendu   ", answer['text'])
@@ -156,29 +174,30 @@ with open(path_data+'dev-v1.1.json', 'r') as input:
                     #     else:
                     #         dict[sentence_positionRelative] = 1
 
+print(diff)
+print(pareil)
 
-
-
+print(time.time() - time0)
 # with open(path_dest+'data_Dev.json', 'w') as outfile:
 #     json.dump(out_json, outfile)
 # with open(path_dest+'data_Dev_withQ.json', 'w') as outfile:
 #     json.dump(out_json, outfile)
 
 
-list_dict_trie =sorted(dict.items(), reverse=False, key=operator.itemgetter(0))
-
-with open('../position_span_words_dev.csv', 'w') as f:
-    item_str = "position relative"+","+"comptage relatif"+","+"comptage"+","+"class" + "\n"
-    f.write(item_str)
-    for item in list_dict_trie:
-        item_str = str(round(max(item[0]-0.05,0.0),2))+"-"+str(round(min(item[0]+0.05,1.0),2))+","+ str(item[1] / float(total_answer))+","+ str(item[1] )+",1" + "\n"
-        # item_str = str(round(item[0], 2)) + "," + str(item[1] / float(total_answer)) + "," + str(item[1]) + ",1" + "\n"
-        # item_str = str(round(item[0],4)) + "," + str(item[1] / float(total_answer)) + "," + str(item[1]) + ",1" + "\n"
-        for k in range(item[1]):
-            f.write(item_str)
-
-
-list_dict_trie = sorted(dict_phrase_position.items(), reverse=False, key=operator.itemgetter(0))
+# list_dict_trie =sorted(dict.items(), reverse=False, key=operator.itemgetter(0))
+#
+# with open('../position_span_words_dev.csv', 'w') as f:
+#     item_str = "position relative"+","+"comptage relatif"+","+"comptage"+","+"class" + "\n"
+#     f.write(item_str)
+#     for item in list_dict_trie:
+#         item_str = str(round(max(item[0]-0.05,0.0),2))+"-"+str(round(min(item[0]+0.05,1.0),2))+","+ str(item[1] / float(total_answer))+","+ str(item[1] )+",1" + "\n"
+#         # item_str = str(round(item[0], 2)) + "," + str(item[1] / float(total_answer)) + "," + str(item[1]) + ",1" + "\n"
+#         # item_str = str(round(item[0],4)) + "," + str(item[1] / float(total_answer)) + "," + str(item[1]) + ",1" + "\n"
+#         for k in range(item[1]):
+#             f.write(item_str)
+#
+#
+# list_dict_trie = sorted(dict_phrase_position.items(), reverse=False, key=operator.itemgetter(0))
 
 # with open('../../extrac_sentence/data_perso/freq_position_train.csv', 'w') as f:
 #     item_str = "position relative"+","+"comptage relatif"+","+"comptage"+","+"class" + "\n"
