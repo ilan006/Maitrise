3b76d00b32099fdb34edafb08441d945ca926ddc
Ilan Elbaz <elbaz.ilan00@gmail.com>
2018-08-14 09:24:03 -0400

    message

--- etude_signal/code/signal-last.py
+++ etude_signal/code/signal-last.py
@@ -121,7 +121,7 @@ def numeral_position(position, text, bool_affichage = False):
 
 sim_moy = 0
 taille_elargissement = 10
-sim_moy = [0] * taille_elargissement
+sim_moy = [0.0] * taille_elargissement
 total_answer = 0
 with open(path_data+'dev-v1.1.json', 'r') as input:
     d = json.load(input)
