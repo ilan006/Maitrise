24eef3157342e79e4ce1337de2f57df27078ae4a
Ilan Elbaz <elbaz.ilan00@gmail.com>
2018-08-22 01:17:52 -0400

    bonjour

--- etude_signal/code/signal-last.py
+++ etude_signal/code/signal-last.py
@@ -151,7 +151,7 @@ with open(path_data+'dev-v1.1.json', 'r') as input:
                         pareil += 1
                         repondu = True
                         vect_avg_question = avg_sentence_vector(question['question'], model)
-                        for elargissement in range(taille_elargissement):
+                        for elargissement in range(taille_elargissement+1):
                             # print(elargissement)
                             span = " ".join(word_tokenize(paragraph['context'])[position_Word+elargissement:position_Word+elargissement+1])
                             # span = " ".join(word_tokenize(paragraph['context'])[position_Word - 10 :position_Word - 9])
