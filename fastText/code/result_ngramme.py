b260bd2c3563a93ea3214162aa4d0f2b61ff81df
Ilan Elbaz <elbaz.ilan00@gmail.com>
2018-08-24 00:36:02 -0400

    bonjour

--- fastText/code/result_ngramme.py
+++ fastText/code/result_ngramme.py
@@ -9,12 +9,12 @@ from nltk import ngrams
 from nltk.stem import PorterStemmer
 ps = PorterStemmer()
 
-model = fastText.load_model('../wiki.simple/wiki.simple.bin')
+model = fastText.load_model('../../../Divers_Data_Maitrise/wiki.simple/wiki.simple.bin')
 #model = fastText.load_model('../embeding_perso_fastText/data_embeding.bin')
 #model = fastText.load_model('../embeding_perso_fastText/train_steam_embeding.bin')
 
-path_data = '../data/'
-path_dest = '../data_perso/'
+path_data = '../../../Data_Maitrise/data/'
+path_dest = '../../../Data_Maitrise/data_perso/'
 
 time1 = time.time()
 
