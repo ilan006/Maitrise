2a5ca80bc0587a2d033baf215e5ad08f474b889f
Ilan Elbaz <elbazila@octal10.iro.umontreal.ca>
2018-09-03 15:02:56 -0400

    bonjour

--- etude_signal/code/test.py
+++
@@ -1,5 +0,0 @@
-import fasttext
-
-# Skipgram model
-model = fasttext.skipgram('data.txt', 'model')
-print(model.words)# list of words in dictionary
--- etude_signal/code/test1.py
+++
@@ -1,9 +0,0 @@
-import fasttext
-
-# Skipgram model
-model = fasttext.skipgram('data.txt', 'model')
-print(model.words) # list of words in dictionary
-
-# CBOW model
-model = fasttext.cbow('data.txt', 'model')
-print(model.words) # list of words in dictionary
