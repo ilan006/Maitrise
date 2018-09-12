5791c093047e0e2fd5b0cb6b80beb0cfe4ab5f76
Ilan Elbaz <elbaz.ilan00@gmail.com>
2018-09-12 18:05:18 -0400

    bonjour

--- etude_signal/code/signal-last.py
+++ etude_signal/code/signal-last.py
@@ -23,7 +23,7 @@ dict = {}
 dict_phrase_position = {}
 len_final = 0
 nb_par = 0
-s
+
 model = fastText.load_model('../../../Divers_Data_Maitrise/wiki.simple/wiki.simple.bin')
 #model = fastText.load_model('../embeding_perso_fastText/data_embeding.bin')
 #model = fastText.load_model('../embeding_perso_fastText/train_steam_embeding.bin')
