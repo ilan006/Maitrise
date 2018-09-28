ac03954ed8956275542738b4e209582c59e2ef4e
Ilan Elbaz <elbaz.ilan00@gmail.com>
2018-09-28 11:43:34 -0400

    bonjour

---
+++ Alignement/code/graph_bipartite.py
@@ -0,0 +1,9 @@
+import networkx as nx
+from networkx.algorithms import bipartite
+import matplotlib.pyplot as plt
+limits = plt.axis('off')
+
+B = nx.Graph()
+B.add_nodes_from([1,2,3,4], bipartite=0) # Add the node attribute "bipartite"
+B.add_nodes_from(['a','b','c'], bipartite=1)
+B.add_edges_from([(1,'a'), (1,'b'), (2,'b'), (2,'c'), (3,'c'), (4,'a')])
