af4e54f0c2dfbe56467bcc0b611d6030b2dc5a9f
Ilan Elbaz <elbaz.ilan00@gmail.com>
2018-09-29 16:19:48 -0400

    bonjour

--- Alignement/code/graph_bipartite.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-from networkx.algorithms import bipartite
-
-B = nx.Graph()
-# Add nodes with the node attribute "bipartite"
-B.add_nodes_from([1, 2, 3, 4], bipartite=0)
-B.add_nodes_from(['a', 'b', 'c'], bipartite=1)
-# Add edges only between nodes of opposite node sets
-B.add_edges_from([(1, 'a'), (1, 'b'), (2, 'b'), (2, 'c'), (3, 'c'), (4, 'a')])
