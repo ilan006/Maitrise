3eb56f320aa59fa04cd1402e8ffd35f8f467b5a5
Ilan Elbaz <elbaz.ilan00@gmail.com>
2019-01-22 17:14:05 -0500

    commit_ilan_Diro

--- Heuristique/code/loss_and_inverted_size_ans_in_ent.py
+++ Heuristique/code/loss_and_inverted_size_ans_in_ent.py
@@ -13,22 +13,13 @@ import sys
 import time
 import spacy
 
+import os
+file_name = os.path.basename(__file__)[:-3]
 
 path_data = '../../../Data_Maitrise/data/'
 path_dest = '../data_results/'
 selected_data = "dev"
 # selected_data = "train"
-
-# type_question = 1 #'Where?'
-# type_question = 2 #'How much / many?'
-# type_question = 3 #'What name / is called?'
-# type_question = 4 #'Who?'
-# type_question = 5 #'When / What year?'
-
-type_question = 2
-
-print("type de la question", type_question)
-print()
 
 
 def interesting_entities(type_question):
@@ -49,9 +40,6 @@ time1 = time.time()
 nlp = spacy.load('en_core_web_sm')
 
 
-
-
-
 list_type_question_interesting = ['Where?', 'How much / many?', 'What name / is called?', 'Who?', 'When / What year?']
 dict_type_question= {}
 for type_question in list_type_question_interesting :
@@ -70,9 +58,6 @@ with open(path_data + selected_data + '-v1.1.json', 'r') as input:
                 num_quest += 1
                 if num_quest % 100 == 0:
                     # print(dict_type_question)
-                    moy_loss_all_types = moy_inverted_size_all_types = total_nb_questions = 0
-
-
                     print(num_quest)
                     # time.sleep(2)
                     
@@ -81,22 +66,30 @@ with open(path_data + selected_data + '-v1.1.json', 'r') as input:
                 list_ent_data = []
                 for ent in nlp_paragraph.ents:
                     if ent.label_ in interesting_entities(type_question):
-                        list_ent_data.append(ent.text)
+                        list_ent_data.append(normalize_answer(ent.text))
                 
                 dict_type_question[type_question][0] += 1
                 concatenation_ent = ' '.join(list_ent_data)
                 for answer in list(set(list(map(lambda x: normalize_answer(x["text"]),question['answers'])))):
-                    if answer in normalize_answer(concatenation_ent) :
+                    if answer in concatenation_ent :
                         dict_type_question[type_question][1] += 1
                         break
                 dict_type_question[type_question][2] += len(concatenation_ent) / float(len(paragraph['context']))
+moy_loss_all_types = moy_inverted_size_all_types = total_nb_questions = 0
+with open(path_dest + file_name + '.csv', 'w') as f:
+    f.write("Question ; nb_de_questions ; perte ; taille_inversee"+ "\n")
+    for key in dict_type_question:
+        moy_loss = 1.0 - (dict_type_question[key][1] / dict_type_question[key][0])
+        moy_inverted_size = dict_type_question[key][2] / dict_type_question[key][0]
+        moy_loss_all_types += moy_loss * dict_type_question[key][0]
+        moy_inverted_size_all_types += moy_inverted_size * dict_type_question[key][0]
+        total_nb_questions += dict_type_question[key][0]
 
-for key in dict_type_question :
-    moy_loss = 1.0 - (dict_type_question[key][1] / dict_type_question[key][0])
-    moy_inverted_size = dict_type_question[key][2] / dict_type_question[key][0]
-    moy_loss_all_types += moy_loss * dict_type_question[key][0]
-    moy_inverted_size_all_types += moy_inverted_size * dict_type_question[key][0]
-    total_nb_questions += dict_type_question[key][0]
-    print(key , "   nb de questions",  dict_type_question[key][0], "    perte:", moy_loss , "    taille_inversee moyenne", moy_inverted_size )
-print("MOYENNE SUR TOUT LES TYPES",  "   nb de questions",  total_nb_questions, "    perte:", moy_loss_all_types/total_nb_questions , "    taille_inversee moyenne", moy_inverted_size_all_types/total_nb_questions )
+
+        item_str = (key, str(dict_type_question[key][0]),str(moy_loss), str(moy_inverted_size))
+        item_str = " ; ".join(item_str) + "\n"
+        f.write(item_str)
+    item_str = ("MOYENNE TOUT TYPE", str(total_nb_questions), str(moy_loss_all_types/total_nb_questions ), str(moy_inverted_size_all_types/total_nb_questions))
+    item_str = " ; ".join(item_str) + "\n"
+    f.write(item_str)
 print("temps execution", time.time()-time1)
